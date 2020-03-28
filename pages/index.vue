f490bf149a8b9fcfcb168da43fff2824906de04e
Herson Cruz <herson@gmail.com>
2020-03-27 18:08:08 -0600

    Module 2, Lesson 16 completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My slick Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section class="container">
+    <nuxt-link to="/users">Users</nuxt-link>
+  </section>
 </template>
 
 <script>
